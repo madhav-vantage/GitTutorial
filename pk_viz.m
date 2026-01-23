e51acd124f8323439bc941976d5af8909bc85356
madhav <madhavpro3@gmail.com>
2026-01-23 16:04:40 +0530

    changed from multiple to subplot

--- pk_viz.m
+++ pk_viz.m
@@ -4,8 +4,9 @@ pkdata=readtable("Data.csv");
 
 
 groups=unique(pkdata.Group);
-for ginx=1:length(groups)
 figure();
+for ginx=1:length(groups)
+    subplot(3,3,ginx);
     rows=pkdata.Group==groups(ginx);
     scatter(pkdata.Time_days(rows),pkdata.Conc_ngml(rows));
 end
