cb1e5ce53fd4ea1c1edca77b219066d9ba940e86
raunak-d <raunak@vantage-research.net>
2026-01-23 16:30:10 +0530

    Update pk_viz.m

--- pk_viz.m
+++ pk_viz.m
@@ -9,5 +9,6 @@ for ginx=1:length(groups)
     subplot(3,3,ginx);
     rows=pkdata.Group==groups(ginx);
     scatter(pkdata.Time_days(rows),pkdata.Conc_ngml(rows));
+    title(strcat("Group name: ", num2str(groups(ginx))));
 end
     
